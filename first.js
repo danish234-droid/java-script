5c55100071a4f40d4c5140f0353e129fc914a8f7
danish234-droid <d9963534@gmail.com>
2026-03-09 16:21:29 +0500

    Initial commit

---
+++ first.js
@@ -0,0 +1,108 @@
+//fullName="Danish ali";
+
+// for-of loop
+
+/*let str="software engineering";
+let size=0;
+
+for( let i of str){
+    console.log("i=",i);
+    size++;
+
+}console.log("size of string=",size);*/
+
+// for-in loop
+/*let obj={
+    name:"Danish",
+    age:20,
+    city:"Karachi"
+}
+for (let key in obj){
+    console.log("key=",key);
+}*/
+
+/*for(let i=0; i<=100; i++){
+    if(i%2!==0){
+    console.log("i=",i);
+    }
+}*/
+
+/*let gameNum= 12;
+let userNum= prompt("guess the number:");
+console.log("usernumber=",userNum);
+
+while( userNum != gameNum){
+    userNum=prompt("you entered wrong number guess the number again:");
+
+}
+console.log( "congatulations you entered correct number");*/
+
+
+/*let str="sayalni mass it program";
+console.log("length of string=",str.length);
+console.log(str[9]);
+
+
+console.log(`this is a special string `,str);*/
+
+//template literals
+
+/*let obj={
+    item:"pen",
+    price:50,
+    color:"blue",
+};
+let output=`The cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);*/
+
+/*let fullName=prompt("Enter your fullName:");
+let userName="@" +fullName+ fullName.length;
+
+console.log(userName);*/
+
+//ARRAYS
+/*let marks=[12,29,19,23,25,20];
+console.log(marks);
+
+for(let index=0; index<marks.length; index++){
+    console.log(marks[index]);
+}*/
+
+/*let marks=[85,97,44,37,76,60];
+let sum=0;
+
+for( let val of marks){
+    sum=sum+ val;
+
+}
+console.log(sum);
+let avg=sum/marks.length;
+console.log(`average marks of the class is ${avg}`);*/
+
+/*let items=[250,645,300,900,50];
+
+for(let i=0; i<items.length; i++){
+    let offer= items[i]/10;
+    items[i]=items[i]-offer; 
+}
+console.log(items);*/
+
+
+/*let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+let deleted= companies.splice(0,1);
+console.log(deleted);
+
+let remove= companies.splice(1,1,'Ola');
+console.log(remove);
+let add=companies.push("Amazone"); 
+console.log(companies);*/
+
+
+
+
+
+    
+
+
+
